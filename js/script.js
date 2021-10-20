7c692fddf6488b7fcff47997ee1be1b082c827b9
Cristian1231233 <campagnacristian22@gmail.com>
2021-10-20 15:01:06 +0200

    completo commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// inserisci nome, cognome e colore preferito
+// output nome, cognome e colore 
+
+/* 
+1) Inizializzo variabili con dati
+2) Scrivo nome, cognome e colore
+*/
+
+const nome = prompt('Come ti chiami?');
+const cognome = prompt('Il tuo cognome?');
+const colorePreferito = prompt('Colore preferito?');
+
+console.log(nome);
+console.log(cognome);
+console.log(colorePreferito);
+
+document.getElementById('stampa').innerHTML = `
+      Nome, Cognome e colore preferito: ${nome} ${cognome} ${colorePreferito} 21
+
+`;
